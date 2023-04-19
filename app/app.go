16d7eb743c313d209b95822107311a6ab7626d53
osawa-koki <github@osawa.dev>
2023-04-19 22:44:35 +0900

    小さな修正。

--- app/app.go
+++ app/app.go
@@ -1,5 +1,24 @@
 package app
 
+import (
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"os"
+)
+
 func Greet() {
-	println("Hello, world!")
+	// TOMLファイルを読み込み
+	content, err := os.ReadFile("config.toml")
+	if err != nil {
+		panic(err)
+	}
+
+	// TOMLファイルを構造体にパース
+	var config Config
+	if _, err := toml.Decode(string(content), &config); err != nil {
+		panic(err)
+	}
+
+	// パース結果を表示
+	fmt.Println("Title:", config.Title)
 }
--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.20
 replace example.com/main/app => ./app
 
 require example.com/main/app v0.0.0-00010101000000-000000000000
+
+require github.com/BurntSushi/toml v1.2.1 // indirect
