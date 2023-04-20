fca4e8965d8a59d02ce369f552a07067c94b6ecd
osawa-koki <github@osawa.dev>
2023-04-20 22:04:28 +0900

    ファイル名一覧を出力。

--- app/app.go
+++ app/app.go
@@ -2,7 +2,10 @@ package app
 
 import (
 	"fmt"
-  "path/filepath"
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/BurntSushi/toml"
 )
 
@@ -14,13 +17,41 @@ func App() {
 		return
 	}
 
+	// ファイル名を格納するリストを定義
+	var files []string
+
 	config.Mandelbrot.Width = config.Global.Width
 	config.Mandelbrot.Height = config.Global.Height
+	files = append(files, config.Mandelbrot.OutputFile)
 	config.Mandelbrot.OutputFile = filepath.Join(config.Global.OutputDirectory, config.Mandelbrot.OutputFile)
 	MandelbrotDrawer(config.Mandelbrot)
 
 	config.Julia.Width = config.Global.Width
 	config.Julia.Height = config.Global.Height
+	files = append(files, config.Julia.OutputFile)
 	config.Julia.OutputFile = filepath.Join(config.Global.OutputDirectory, config.Julia.OutputFile)
 	JuliaDrawer(config.Julia)
+
+	{ /* ファイル名一覧を出力 */
+		// 新しいファイルのパス
+		outputFile := filepath.Join(config.Global.OutputDirectory, "items.txt")
+
+		// 新しいファイルを作成
+		out, err := os.Create(outputFile)
+		if err != nil {
+			panic(err)
+		}
+		defer out.Close()
+
+		// スライスの要素を改行で結合して新しいファイルに書き込む
+		line := strings.Join(files, "\n")
+		_, err = out.WriteString(line)
+		if err != nil {
+			panic(err)
+		}
+
+		if err := out.Sync(); err != nil {
+			panic(err)
+		}
+	}
 }
